d400e95e08bc7a17be218edef532abd30fa1ed68
Денис <@HOME>
2020-06-02 20:14:17 +0300

    passing parameters using POST

---
+++ ServletsStart/src/main/java/d/bozhevilnyi/PassingParametersUsingPostMethodDemo.java
@@ -0,0 +1,33 @@
+package d.bozhevilnyi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PassingParametersUsingPostMethodDemo extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		
+		PrintWriter writer = resp.getWriter();
+		String title = "Passing parameters using POST method";
+		String docType = "<!DOCTYPE html>";
+		
+		writer.println("<html>" + 
+				"<head><title>" + title + "</title></head>\n" +
+				"<body><h2>Specialty: </h2>" + req.getParameter("specialty") + 
+				"<h2>Experience: </h2>" + req.getParameter("experience") + 
+				"</body>" + 
+				"</html>");
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+	
+
+}
